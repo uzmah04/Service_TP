3333be85fbc6f34c442119005ff8756b248388cf
uzmah04 <zu.goomany@gmail.com>
2020-09-20 16:22:09 +0400

    ****

--- check_decrypt.sh
+++ check_decrypt.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-while [ true ]
+cd /home/uzmah/src/Service_TP/toDecrypt
+array=($(echo *))
+
+cd /home/uzmah/src/Service_TP/Decrypted
+In_Decrypted="$(echo *)"
+
+for fichier in ${array[*]}
 do
-	cd /home/uzmah/src/Service_TP/toDecrypt
-	array=($(echo *))
-
-	cd /home/uzmah/src/Service_TP/Decrypted
-	In_Decrypted="$(echo *)"
-
-	for fichier in ${array[*]}
-	do
-        	if [[ "$In_Decrypted" == "$fichier.decrypted" ]]
-        	then
-                	echo "present $fichier"
-        	else
-                	cd /home/uzmah/src/Service_TP
-                	python3 decrypt.py "$fichier"
-                	mv /home/uzmah/src/Service_TP/toDecrypt/"$fichier.decrypted" /home/uzmah/src/Service_TP/Decrypted/"$fichier.decrypted"
-        	fi
-	done
+       	if [[ "$In_Decrypted" == "$fichier.decrypted" ]]
+       	then
+               	echo "present $fichier"
+       	else
+               	cd /home/uzmah/src/Service_TP
+               	python3 decrypt.py "$fichier"
+               	mv /home/uzmah/src/Service_TP/toDecrypt/"$fichier.decrypted" /home/uzmah/src/Service_TP/Decrypted/"$fichier.decrypted"
+       	fi
 done
--- check_encrypt.sh
+++ check_encrypt.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-while [ true ]
+cd /home/uzmah/src/Service_TP/toCrypt
+array=($(echo *))
+
+cd /home/uzmah/src/Service_TP/Crypted
+In_Crypted="$(echo *)"
+
+for fichier in ${array[*]}
 do
-	cd /home/uzmah/src/Service_TP/toCrypt
-	array=($(echo *))
-
-	cd /home/uzmah/src/Service_TP/Crypted
-	In_Crypted="$(echo *)"
-
-	for fichier in ${array[*]}
-	do
-		if [[ "$In_Crypted" == "$fichier.encrypted" ]]
-		then
-			echo "present $fichier"
-		else
-			cd /home/uzmah/src/Service_TP
-			python3 encrypt.py "$fichier"
-			mv home/uzmah/src/Service_TP/toCrypt/"$fichier.encrypted" home/uzmah/src/Service_TP/Crypted/"$fichier.encrypted"
-		fi
-	done
+	if [[ "$In_Crypted" == "$fichier.encrypted" ]]
+	then
+		echo "present $fichier"
+	else
+		cd /home/uzmah/src/Service_TP
+		python3 encrypt.py "$fichier"
+		mv home/uzmah/src/Service_TP/toCrypt/"$fichier.encrypted" home/uzmah/src/Service_TP/Crypted/"$fichier.encrypted"
+	fi
 done
